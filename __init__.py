4114fd995750e2a0dc8959ff89cfd7cd2de48b53
ivan_grigorev <rauscral@gmail.com>
2020-04-12 14:25:44 +0300

    create form(wtfform) login user, create templat login-user and rout-login, install Flask-login and add method UserMixin check_password user

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from flask import render_template
 from app.weather import weather_by_city
 from app.news import get_data_news
 from app.model import db
+from app.forms import LoginForm
 from flask import current_app
 
 def create_app(): 
@@ -14,18 +15,25 @@ def create_app():
     @app.route('/')
     def index():
         title = "Flask"
-        conent_title = "Flask Blog"
-        return render_template ("index.html", title=title, conent_title=conent_title)
+        content_title = "Flask Blog"
+        weather = weather_by_city(current_app.config["WEATHER_DEFAULT_CITY"])
+        return render_template ("index.html", title=title, content_title=content_title, weather=weather)
 
     @app.route('/news')
     def news():
         title = "Новости"
-        conent_title = "Новости Python"
+        content_title = "Новости Python"
         news_list = get_data_news()
-        weather = weather_by_city(current_app.config["WEATHER_DEFAULT_CITY"])
-        return render_template ("news.html", title=title, weather=weather, 
-                                newslist=news_list, conent_title=conent_title
+        
+        return render_template ("news.html", title=title, 
+                                newslist=news_list, content_title=content_title
                                 )
+    
+    @app.route("/login")
+    def login():
+        title = "Авторизация"
+        form = LoginForm()
+        return render_template("login.html", form=form, title=title)
 
 
     return app

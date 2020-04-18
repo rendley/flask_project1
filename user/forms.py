eb75422b155624cbb3268b891ef51d3e8da39e55
ivan_grigorev <rauscral@gmail.com>
2020-04-18 04:20:21 +0300

    validators check double username, double email

--- user/forms.py
+++ user/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, EqualTo, Email
 
+from wtforms import ValidationError
+from app.user.models import User
 
 
 # login User
@@ -18,3 +20,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()], render_kw={"class": "form-control"})
     password2 = PasswordField("Повторите Password", validators=[DataRequired(), EqualTo("password")], render_kw={"class": "form-control"}) 
     submit = SubmitField("Отправить", render_kw={"class": "btn btn-outline-info"})
+
+    # my validators check doble user
+    def validate_username(self, username):
+        double_user = User.query.filter_by(username=username.data).count()
+        if double_user > 0:
+            raise ValidationError("Пользователь с таким именем уже существует!")
+    
+    # my validators check doble email
+    def validate_email(self, email):
+        double_email = User.query.filter_by(email=email.data).count()
+        if double_email > 0:
+            raise ValidationError("Пользователь с таким email уже существует!")
+
